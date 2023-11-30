03fccd1ca73a81dcbee03c8cb6ca9a51fc85eaeb
Prasad <mmpchathuranga.07@gmail.com>
2023-11-30 08:33:00 +0530

    Create downloader script using huggingface_hub

--- model_downloader.py
+++ model_downloader.py
@@ -1,10 +1,34 @@
 # Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from huggingface_hub import hf_hub_download
 
-model_name = "TinyPixel/Llama-2-7B-bf16-sharded"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
 
-save_directory = "llama-2-7b-bf16/"
-model.save_pretrained(save_directory)
-tokenizer.save_pretrained(save_directory)
+files = [
+    ".gitattributes",
+    "config.json",
+    "generation_config.json",
+    "pytorch_model-00001-of-000014.bin",
+    "pytorch_model-00002-of-000014.bin",
+    "pytorch_model-00003-of-000014.bin",
+    "pytorch_model-00004-of-000014.bin",
+    "pytorch_model-00005-of-000014.bin",
+    "pytorch_model-00006-of-000014.bin",
+    "pytorch_model-00007-of-000014.bin",
+    "pytorch_model-00008-of-000014.bin",
+    "pytorch_model-00009-of-000014.bin",
+    "pytorch_model-00010-of-000014.bin",
+    "pytorch_model-00011-of-000014.bin",
+    "pytorch_model-00012-of-000014.bin",
+    "pytorch_model-00013-of-000014.bin",
+    "pytorch_model-00014-of-000014.bin",
+    "pytorch_model.bin.index.json",
+    "special_tokens_map.json",
+    "tokenizer.json",
+    "tokenizer.model",
+    "tokenizer_config.json"
+]
+
+repo_id = "TinyPixel/Llama-2-7B-bf16-sharded"
+output_dir = "llama-2-7b-bf16/"
+
+for filename in files:
+    hf_hub_download(repo_id=repo_id, filename=filename, local_dir=output_dir)
