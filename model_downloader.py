6f6eac08195bad5f6bd53f0c057dbf1d08243a60
Prasad <mmpchathuranga.07@gmail.com>
2024-01-23 06:33:17 +0530

    Modify the downloader code

--- model_downloader.py
+++ model_downloader.py
@@ -1,33 +1,38 @@
-# Load model directly
-from huggingface_hub import hf_hub_download
+from huggingface_hub import snapshot_download
+
+snapshot_download(repo_id="microsoft/phi-2",
+                  revision="834565c23f9b28b96ccbeabe614dd906b6db551a")
+
+# # Load model directly
+# from huggingface_hub import hf_hub_download
 
 
-# files = [
-#     ".gitattributes",
-#     "config.json",
-#     "generation_config.json",
-#     "pytorch_model-00001-of-00014.bin",
-#     "pytorch_model-00002-of-00014.bin",
-#     "pytorch_model-00003-of-00014.bin",
-#     "pytorch_model-00004-of-00014.bin",
-#     "pytorch_model-00005-of-00014.bin",
-#     "pytorch_model-00006-of-00014.bin",
-#     "pytorch_model-00007-of-00014.bin",
-#     "pytorch_model-00008-of-00014.bin",
-#     "pytorch_model-00009-of-00014.bin",
-#     "pytorch_model-00010-of-00014.bin",
-#     "pytorch_model-00011-of-00014.bin",
-#     "pytorch_model-00012-of-00014.bin",
-#     "pytorch_model-00013-of-00014.bin",
-#     "pytorch_model-00014-of-00014.bin",
-#     "pytorch_model.bin.index.json",
-#     "special_tokens_map.json",
-#     "tokenizer.json",
-#     "tokenizer.model",
-#     "tokenizer_config.json"
-# ]
+# # files = [
+# #     ".gitattributes",
+# #     "config.json",
+# #     "generation_config.json",
+# #     "pytorch_model-00001-of-00014.bin",
+# #     "pytorch_model-00002-of-00014.bin",
+# #     "pytorch_model-00003-of-00014.bin",
+# #     "pytorch_model-00004-of-00014.bin",
+# #     "pytorch_model-00005-of-00014.bin",
+# #     "pytorch_model-00006-of-00014.bin",
+# #     "pytorch_model-00007-of-00014.bin",
+# #     "pytorch_model-00008-of-00014.bin",
+# #     "pytorch_model-00009-of-00014.bin",
+# #     "pytorch_model-00010-of-00014.bin",
+# #     "pytorch_model-00011-of-00014.bin",
+# #     "pytorch_model-00012-of-00014.bin",
+# #     "pytorch_model-00013-of-00014.bin",
+# #     "pytorch_model-00014-of-00014.bin",
+# #     "pytorch_model.bin.index.json",
+# #     "special_tokens_map.json",
+# #     "tokenizer.json",
+# #     "tokenizer.model",
+# #     "tokenizer_config.json"
+# # ]
 
-repo_id = "microsoft/phi-2"
-output_dir = "microsoft/phi-2"
+# repo_id = "microsoft/phi-2"
+# output_dir = "microsoft/phi-2"
 
-hf_hub_download(repo_id=repo_id, local_dir=output_dir)
+# hf_hub_download(repo_id=repo_id, local_dir=output_dir)
