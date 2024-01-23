cd509687d599f83cf512b6bff47fa1565fc61772
Prasad <mmpchathuranga.07@gmail.com>
2024-01-23 06:16:09 +0530

    Phi-2 downloader script

--- model_downloader.py
+++ model_downloader.py
@@ -2,33 +2,32 @@
 from huggingface_hub import hf_hub_download
 
 
-files = [
-    ".gitattributes",
-    "config.json",
-    "generation_config.json",
-    "pytorch_model-00001-of-00014.bin",
-    "pytorch_model-00002-of-00014.bin",
-    "pytorch_model-00003-of-00014.bin",
-    "pytorch_model-00004-of-00014.bin",
-    "pytorch_model-00005-of-00014.bin",
-    "pytorch_model-00006-of-00014.bin",
-    "pytorch_model-00007-of-00014.bin",
-    "pytorch_model-00008-of-00014.bin",
-    "pytorch_model-00009-of-00014.bin",
-    "pytorch_model-00010-of-00014.bin",
-    "pytorch_model-00011-of-00014.bin",
-    "pytorch_model-00012-of-00014.bin",
-    "pytorch_model-00013-of-00014.bin",
-    "pytorch_model-00014-of-00014.bin",
-    "pytorch_model.bin.index.json",
-    "special_tokens_map.json",
-    "tokenizer.json",
-    "tokenizer.model",
-    "tokenizer_config.json"
-]
+# files = [
+#     ".gitattributes",
+#     "config.json",
+#     "generation_config.json",
+#     "pytorch_model-00001-of-00014.bin",
+#     "pytorch_model-00002-of-00014.bin",
+#     "pytorch_model-00003-of-00014.bin",
+#     "pytorch_model-00004-of-00014.bin",
+#     "pytorch_model-00005-of-00014.bin",
+#     "pytorch_model-00006-of-00014.bin",
+#     "pytorch_model-00007-of-00014.bin",
+#     "pytorch_model-00008-of-00014.bin",
+#     "pytorch_model-00009-of-00014.bin",
+#     "pytorch_model-00010-of-00014.bin",
+#     "pytorch_model-00011-of-00014.bin",
+#     "pytorch_model-00012-of-00014.bin",
+#     "pytorch_model-00013-of-00014.bin",
+#     "pytorch_model-00014-of-00014.bin",
+#     "pytorch_model.bin.index.json",
+#     "special_tokens_map.json",
+#     "tokenizer.json",
+#     "tokenizer.model",
+#     "tokenizer_config.json"
+# ]
 
-repo_id = "TinyPixel/Llama-2-7B-bf16-sharded"
-output_dir = "llama-2-7b-bf16/"
+repo_id = "microsoft/phi-2"
+output_dir = "microsoft/phi-2"
 
-for filename in files:
-    hf_hub_download(repo_id=repo_id, filename=filename, local_dir=output_dir)
+hf_hub_download(repo_id=repo_id, local_dir=output_dir)
