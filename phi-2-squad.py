e85af3fde75505e219b4be14ee7d8c64b03cf707
Prasad <mmpchathuranga.07@gmail.com>
2024-01-25 08:59:35 +0530

    Change the fp16 config

--- phi-2-squad.py
+++ phi-2-squad.py
@@ -163,8 +163,8 @@ training_args = TrainingArguments(
     weight_decay=0.01,
     # basically just train for 5 epochs, you should train for longer
     max_steps=int(len(training_data) * 1),
-    warmup_steps=150,
-    fp16=True,
+    warmup_steps=100,
+    # fp16=True,
     # tf32=True,
     gradient_checkpointing=True,
     # max_grad_norm=0.3,  # from the paper
