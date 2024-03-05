7d2e921121405c3781dc064e548de3ba2ce6571f
Prasad <mmpchathuranga.07@gmail.com>
2024-03-06 00:58:12 +0530

    Configure PEFT

--- fine-tuning-scripts/llama-2-7b-clm.py
+++ fine-tuning-scripts/llama-2-7b-clm.py
@@ -1,4 +1,4 @@
-from peft import LoraConfig
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, AutoPeftModelForCausalLM
 from huggingface_hub import login
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments, Trainer, DataCollatorForLanguageModeling
 from datasets import Dataset
@@ -13,15 +13,14 @@ model_name = "../Llama-2-7b-chat-hf"
 
 llama2 = AutoModelForCausalLM.from_pretrained(
     model_name,
-    # torch_dtype=torch.float16,
+    torch_dtype=torch.float16,
     device_map="auto",
     trust_remote_code=True,
-    load_in_4bit=True,
-    # quantization_config=BitsAndBytesConfig(
-    #     load_in_4bit=True,
-    #     bnb_4bit_compute_dtype=torch.float16,
-    #     bnb_4bit_quant_type="nf4",
-    # ),
+    quantization_config=BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_quant_type="nf4",
+    ),
     # revision='834565c23f9b28b96ccbeabe614dd906b6db551a'
 )
 
@@ -57,24 +56,46 @@ data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
 
 peft_config = LoraConfig(
-    lora_alpha=16,
-    lora_dropout=0.1,
-    r=64,
+    r=16,  # dimension of the updated matrices
+    lora_alpha=64,  # parameter for scaling
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+        "lm_head",
+    ],
+    lora_dropout=0.1,  # dropout probability for layers
     bias="none",
     task_type="CAUSAL_LM",
 )
 
-llama2.add_adapter(peft_config)
+llama2.gradient_checkpointing_enable()
+llama2 = prepare_model_for_kbit_training(llama2)
+llama2 = get_peft_model(llama2, peft_config)
 
 # Login to the Hugging Face Hub
 login(token="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")
 
 training_args = TrainingArguments(
-    output_dir="llama-2-7b-clm-model",
-    evaluation_strategy="epoch",
+    evaluation_strategy="steps",
+    do_eval=True,
+    auto_find_batch_size=True,  # this is risky
+    # per_device_train_batch_size=16,
+    log_level="debug",
+    # optim="paged_adamw_32bit",
+    optim="paged_adamw_32bit",
+    save_steps=1000,
+    logging_steps=100,
     learning_rate=1e-6,
     weight_decay=0.01,
-    push_to_hub=True,
+    # basically just train for 5 epochs, you should train for longer
+    max_steps=int(len(train_data) * 1),
+    warmup_steps=350,
+    gradient_checkpointing=True,
 )
 
 trainer = Trainer(
@@ -87,4 +108,25 @@ trainer = Trainer(
 
 trainer.train()
 
-llama2.save_pretrained('llama-2-7b-clm-model')
+trainer.save_state()
+trainer.model.save_pretrained('llama-2-7b-clm-model')
+
+
+tuned_llama2_model = AutoPeftModelForCausalLM.from_pretrained(
+    training_args.output_dir,
+    torch_dtype=torch.float16,
+    # torch_dtype='auto',
+    trust_remote_code=True,
+    device_map='auto',
+    offload_folder="offload/"
+)
+
+merged_model = tuned_llama2_model.merge_and_unload()
+
+merged_model.save_pretrained("llama-2-merged_model", safe_serialization=True)
+tokenizer.save_pretrained("llama-2-merged_model")
+
+hf_model_repo = 'mmpc/llama-2-7b-clm'
+# push merged model to the hub
+merged_model.push_to_hub(hf_model_repo)
+tokenizer.push_to_hub(hf_model_repo)
