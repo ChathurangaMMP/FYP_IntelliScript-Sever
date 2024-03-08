79f4831e82ef407df8a5ea682673ab5ec15fb925
Prasad <mmpchathuranga.07@gmail.com>
2024-03-09 00:38:17 +0530

    Chnage the extractor code for a single xlsx file

--- data_extractor.py
+++ data_extractor.py
@@ -4,8 +4,11 @@ from extractor import Extractor
 
 extractor = Extractor()
 
-source_dir = "../CBSL-data/"
-output_dir = "../Extracted-text-CBSL-data-new/"
+# source_dir = "../CBSL-data/"
+# output_dir = "../Extracted-text-CBSL-data-new/"
+
+source_dir = "."
+output_dir = "."
 count = 0
 error_count = 0
 
@@ -26,29 +29,30 @@ for root, directories, files in os.walk(source_dir):
         os.makedirs(output_subdir, exist_ok=True)
 
     for file in files:
-        try:
-            input_file_path = os.path.join(root, file)
+        if file.endswith('.xlsx'):
+            try:
+                input_file_path = os.path.join(root, file)
 
-            # Determine the output file path
-            output_file_path = os.path.join(
-                output_dir, os.path.relpath(root, source_dir), file[:file.rfind('.')+1] + 'txt')
+                # Determine the output file path
+                output_file_path = os.path.join(
+                    output_dir, os.path.relpath(root, source_dir), file[:file.rfind('.')+1] + 'txt')
 
-            if not os.path.exists(output_file_path) and input_file_path not in error_filenames:
-                text = extractor.extractor(input_file_path)
+                if not os.path.exists(output_file_path) and input_file_path not in error_filenames:
+                    text = extractor.extractor(input_file_path)
 
-                # Write the extracted text to the output file
-                with open(output_file_path, 'w', encoding='utf-8') as write_file:
-                    write_file.write(text)
+                    # Write the extracted text to the output file
+                    with open(output_file_path, 'w', encoding='utf-8') as write_file:
+                        write_file.write(text)
 
-                count += 1
-                print(
-                    f"{count} - {file} is extracted and saved to {output_file_path}")
+                    count += 1
+                    print(
+                        f"{count} - {file} is extracted and saved to {output_file_path}")
 
-        except Exception as e:
-            error_count += 1
-            print(f"{error_count}E - {e} - {input_file_path}")
-            with open(error_file_path, 'a', encoding='utf-8') as error_file_write:
-                error_file_write.write(f"{e} - {input_file_path}\n")
+            except Exception as e:
+                error_count += 1
+                print(f"{error_count}E - {e} - {input_file_path}")
+                with open(error_file_path, 'a', encoding='utf-8') as error_file_write:
+                    error_file_write.write(f"{e} - {input_file_path}\n")
 
 
 print(f"Success: {count} - Errors: {error_count}")
